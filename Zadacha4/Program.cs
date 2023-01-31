de2b1643b7b06f8c6c44bd84542becb88c0d1763
Roman Pushnikov <r.pushnikov@mail.ru>
2023-02-01 00:26:16 +0300

    Z4commit

---
+++ Zadacha4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите 1-oe число и нажмите 'Enter': ");
+int a = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Введите 2-oe число и нажмите 'Enter': ");
+int b = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Введите 3-oe число и нажмите 'Enter': ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+{
+    Console.WriteLine($"Max number = {max}.");
+}
+
