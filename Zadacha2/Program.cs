4cb88ca7e2cf2c68b94f259147396465dc64cd26
Roman Pushnikov <r.pushnikov@mail.ru>
2023-02-01 00:15:56 +0300

    innitial commit

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введиет первое число и нажмите 'Enter': ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введиет второе число и нажмите 'Enter': ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+    if (a > b)
+{
+    Console.WriteLine($"max = {a}, min = {b}");
+}
+    if (a < b)
+{
+    Console.WriteLine($"max = {b}, min = {a}");
+}
+    if (a == b)
+{
+    Console.WriteLine("Они одинаковые!");
+}
