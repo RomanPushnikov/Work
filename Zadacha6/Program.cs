a61704bcda0a7694c370aa07ce7fe8fa9bf7a1a3
Roman Pushnikov <r.pushnikov@mail.ru>
2023-02-01 00:32:47 +0300

    Z6 commit

---
+++ Zadacha6/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+string? str =(Console.ReadLine());
+int a;
+Console.WriteLine(int.TryParse(str, out a));
+int  b = (a % 2);
+
+
+switch (b) {
+
+case 0:
+System.Console.WriteLine( $" {a} четное число" );
+break;
+
+default:
+System.Console.WriteLine( $"{a} нечетное число" );
+break;
+}
