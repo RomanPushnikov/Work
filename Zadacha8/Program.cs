fb328619008fbbd4247da552029fb6470b7bc5d4
Roman Pushnikov <r.pushnikov@mail.ru>
2023-02-01 00:42:38 +0300

    Z8 commit

---
+++ Zadacha8/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+string? str = (Console.ReadLine());
+int a;
+Console.WriteLine(int.TryParse(str, out a));
+int b = 1;
+Console.Write($"Чётные числа в диапазоне от 1 до {a}: ");
+
+while (b <= a)
+{
+    if (b % 2 == 0)
+    {
+        Console.Write($"{b}, ");
+        b = b + 1;
+    }
+    else
+    {
+        b = b + 1;
+    }
+
+}
