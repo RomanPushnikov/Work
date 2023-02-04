2d65518c58f14cc8f70e15ae727ca5c7a1d42e7b
Roman Pushnikov <r.pushnikov@mail.ru>
2023-02-04 22:27:51 +0300

    povtornyi zaliv

--- Zadachaa10/Program.cs
+++ Zadachaa10/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Принимает *** число и выдает вторую цифру этого числа.
+Console.Clear();
+
+Console.Write("Введите трехзначное число: ");
+int a;
+string? str = (Console.ReadLine());
+bool result = int.TryParse(str, out a); // bool хранит true или False, "int.TryParse" преобразует строку в целое число и выводит Тrue или False, отсюда и возьмём.
+
+if (result == true & a > 99 & a < 1000)
+{
+    a = a / 10 % 10;
+    Console.WriteLine($"Вторая цифра: {a}.");
+}
+else 
+{
+    Console.WriteLine("Проверьте правильность ввода, и повторите попытку!");
+}
+
