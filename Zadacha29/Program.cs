be77a979240cd7c37a2ccb1975ddc9d6e54516de
Roman Pushnikov <r.pushnikov@mail.ru>
2023-02-14 09:56:05 +0300

    initial commit

---
+++ Zadacha29/Program.cs
@@ -0,0 +1,27 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length){
+        collection[index] = new Random().Next(1, 8);
+        index++;
+    }
+}
+
+// метод void кот будет печатать массив
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+
+}
+
+int[] Array = new int[8];
+FillArray(Array);
+PrintArray(Array);
+
