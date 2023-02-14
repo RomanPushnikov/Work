7ee08bf4db235a7c001303330adf45eb26937fc5
Roman Pushnikov <r.pushnikov@mail.ru>
2023-02-14 09:23:14 +0300

    Innitial commt

--- Zadacha27/Program.cs
+++ Zadacha27/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+ int newNumber()  // Конструкция Функции Ввода разных параметров.
+{
+    System.Console.WriteLine("Введите параметр: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+ int a = newNumber();
+
+
+ int PrintImage(int numb)
+{
+    int sumNumbers = 0;
+    while (numb > 0)
+    {
+        sumNumbers = sumNumbers + numb % 10;
+        numb = numb / 10; 
+    }
+    return sumNumbers;
+}
+
+int res = PrintImage(a); 
+Console.WriteLine(res);
+
