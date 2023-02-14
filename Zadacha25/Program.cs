baec682f158bc0e0230d831e9c3e6e24d68d7733
Roman Pushnikov <r.pushnikov@mail.ru>
2023-02-14 09:19:33 +0300

    sec ocmmit

--- Zadacha25/Program.cs
+++ Zadacha25/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.Clear();
+
+int Method1(int first, int sec)
+{
+    int resPow = (int)Math.Pow(first, sec);
+    return(resPow);
+}
+
+int newNumber()  // Конструкция Функции Ввода разных параметров.
+{
+    System.Console.WriteLine("Введите параметр: ");
+    int number = Convert.ToInt32 (Console.ReadLine());
+    return number;
+
+}
+
+int a = newNumber();
+int b = newNumber();
+
+int pow = Method1(a, b);
+Console.WriteLine($"Первое число в степени второго: {pow}");  
