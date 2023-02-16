624dde3520bdd31de1339e6582e76d79f2a999ef
Roman Pushnikov <r.pushnikov@mail.ru>
2023-02-16 22:05:35 +0300

    innitial commit

---
+++ Zadacha34/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] array = new int[5];
+
+Random rnd = new Random();
+for (int i = 0; i < array.Length; i++) // Сгенерировали массив
+{
+    array[i] = rnd.Next(100, 1000);
+    Console.Write(array[i] + " "); // Вывели массив
+}
+
+int numbEvenNumb = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        numbEvenNumb += 1;
+    }
+}
+
+Console.WriteLine();
+Console.Write($"Количестов четных числе в массиве: {numbEvenNumb}");
