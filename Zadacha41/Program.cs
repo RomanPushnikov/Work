6893c3653710382f14f5ceac8901023062eb3355
Roman Pushnikov <r.pushnikov@mail.ru>
2023-02-21 09:03:20 +0300

    initial commit

---
+++ Zadacha41/Program.cs
@@ -0,0 +1,35 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Clear();
+
+int moreThen0 = 0;
+
+Console.Write("Введите необходимые числа: ");
+string? str = Console.ReadLine(); // вводим строку
+var array = str.Split(", "); // строку делим по пробелу
+
+int[] arrayInt = new int[array.Length]; // переводим в int
+
+for (int i = 0; i < array.Length; i++) // Сгенерировали массив
+{
+    arrayInt[i] = Convert.ToInt32(array[i]);
+}
+
+Console.Write("Массив: ");
+
+foreach (var item in arrayInt)
+{
+    Console.Write(" " +  item);
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (arrayInt [i] > 0) 
+    {
+        moreThen0 += 1;
+    }
+}
+
+Console.Write("Количество чисел > 0: " + moreThen0);
