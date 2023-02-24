8517674c71550447f9add98e64e770c49f35dea2
Roman Pushnikov <r.pushnikov@mail.ru>
2023-02-24 09:24:52 +0300

    initial commit

---
+++ Zadacha50/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+//и возвращает значение этого элемента или же указание, что такого элемента нет
+
+//Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+void PrintImage(int[,] matrix)
+{
+    Console.Write("Enter search number: ");
+    int a;
+    string? str = (Console.ReadLine());
+    bool result = int.TryParse(str, out a);
+    bool b = false;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(matrix[i,j] == a && result == true) 
+            {
+                b = true;
+                break;
+            }
+        }
+    }
+    if (b)                                       // дальше ели b - True то пишу есть.
+    {
+        Console.Write($"This array have that number: {a} ");
+    }                                          // если остался False то пишу нету.
+    else Console.Write("This array haven't that number! "); 
+
+}
+
+int[,] matrix = new int[,]
+{
+{1, 4, 7, 2},
+{5, 9, 2, 3},
+{8, 4, 17, 4},
+};
+
+PrintImage(matrix);
