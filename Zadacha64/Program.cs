c23d3a3a00cc762a96accaed26ac730bcb894beb
Roman Pushnikov <r.pushnikov@mail.ru>
2023-03-03 01:53:49 +0300

    innitial commit

---
+++ Zadacha64/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.Clear();
+
+int newNumber()  // Конструкция Функции Ввода разных параметров.
+{
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void VuvodCelih(int n)
+{
+    if(n > 0) 
+    {
+        Console.Write($"{n}, ");
+        VuvodCelih(n-1);
+    } 
+   
+}
+
+System.Console.Write("Поиск всех натуральных чисел в промежутке от n до 1: n = ");
+int n = newNumber();
+VuvodCelih(n);
